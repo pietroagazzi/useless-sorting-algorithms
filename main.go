8e4e7c97970fbe122bdba67de9aa949153614a28
Pietro Agazzi <contact@agazzipietro.it>
2023-12-12 11:30:55 +0100

    Created Sleepsort and main.go

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/alexflint/go-arg"
+)
+
+var args struct {
+	Lenght    int    `arg:"-l,--lenght" help:"number of elements to sort" default:"10" placeholder:"<n>"`
+	Algorithm string `arg:"-a,--algorithm" help:"algorithm to use" default:"bogosort" placeholder:"<algorithm>"`
+	List      bool   `arg:"-L,--list" help:"list all algorithms"`
+}
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	arg.MustParse(&args)
+
+	fmt.Println("Useless sorting algorithms!")
+}
+
+func AdaptSleepsort(arr []int) []int {
+	var uintArr []uint
+
+	for _, num := range arr {
+		uintArr = append(uintArr, uint(num))
+	}
+
+	result := SleepSort(uintArr)
+
+	var adaptedResult []int
+
+	for _, num := range result {
+		adaptedResult = append(adaptedResult, int(num))
+	}
+
+	return adaptedResult
+}
+
+func main() {
+	var algorithms = map[string]func([]int) []int{
+		"bogosort":   Bogosort,
+		"stalinsort": StalinSort,
+		"sleepsort":  AdaptSleepsort,
+	}
+
+	if args.List {
+		fmt.Println("List of useless sorting algorithms:")
+		for k := range algorithms {
+			fmt.Println("\t-", k)
+		}
+		os.Exit(0)
+	}
+
+	var arr []int
+
+	if _, ok := algorithms[args.Algorithm]; !ok {
+		fmt.Println("Invalid algorithm. Use -L to list all algorithms")
+		os.Exit(1)
+	}
+
+	if args.Lenght < 1 {
+		fmt.Println("Lenght must be greater than 0")
+		os.Exit(1)
+	}
+
+	var max = int(math.Pow(float64(args.Lenght), 2))
+
+	for i := 0; i < args.Lenght; i++ {
+		arr = append(arr, rand.Intn(max))
+	}
+
+	fmt.Printf("Running %s with %d elements\n\nInput: %v\n", args.Algorithm, args.Lenght, arr)
+
+	start := time.Now()
+
+	result := algorithms[args.Algorithm](arr)
+
+	elapsed := time.Since(start)
+
+	fmt.Printf("Output: %v\n", result)
+	fmt.Printf("Is sorted? %t\n", IsSorted(result))
+
+	fmt.Printf("\nElapsed time: %s\n", elapsed)
+}
---
+++ sleepsort.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func Thread(el uint, wg *sync.WaitGroup, ch chan uint) {
+	defer wg.Done()
+	time.Sleep(time.Duration(el) * (time.Second))
+	ch <- el
+}
+
+func SleepSort(arr []uint) []uint {
+	var sorted []uint
+	var wg sync.WaitGroup
+	ch := make(chan uint)
+
+	for _, e := range arr {
+		wg.Add(1)
+		go Thread(e, &wg, ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for el := range ch {
+		sorted = append(sorted, el)
+	}
+
+	return sorted
+}
